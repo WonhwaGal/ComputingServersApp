5af0a3ea4989c2fa2cb1f97b64b95ab16e6d044f
WonhwaGal <110241714+WonhwaGal@users.noreply.github.com>
2026-04-10 21:10:03 +0300

    git_ignore_fix

---
+++ ComputingServers.Application/CQRS/Servers/ReleaseServer/ReleaseServerCommand.cs
@@ -0,0 +1,6 @@
+﻿using ComputingServers.Domain.Results;
+using MediatR;
+
+namespace ComputingServers.Application.CQRS.Servers.Release;
+
+public sealed record ReleaseServerCommand(string Id) : IRequest<Result<bool>>;
---
+++ ComputingServers.Application/CQRS/Servers/ReleaseServer/ReleaseServerCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using ComputingServers.Application.Abstractions;
+using ComputingServers.Application.Repositories;
+using ComputingServers.Domain.Enums;
+using ComputingServers.Domain.Results;
+using ComputingServers.Domain.Results.Errors;
+using MediatR;
+
+namespace ComputingServers.Application.CQRS.Servers.Release;
+
+internal class ReleaseServerCommandHandler(
+	IServerRepository serverRepository,
+	IUnitOfWork unitOfWork,
+	ILoggerManager loggerManager) : IRequestHandler<ReleaseServerCommand, Result<bool>>
+{
+	public async Task<Result<bool>> Handle(ReleaseServerCommand request, CancellationToken cancellationToken)
+	{
+		try
+		{
+			var server = await serverRepository.GetByIdAsync(request.Id);
+			if (server is null)
+				return Result.Failure<bool>(ServerErrors.NotFound(request.Id));
+
+			server.IsAvailable = true;
+			server.LastUsed = DateTime.UtcNow;
+			server.Status = ServerStatus.PoweredOff;
+
+			await unitOfWork.SaveChangesAsync();
+
+			loggerManager.LogInfo($"Server with ID {server.Id} was successfully released.");
+			return Result.Success(true);
+		}
+		catch (Exception ex)
+		{
+			loggerManager.LogError($"Failure when releasing server: {ex.Message}");
+			return Result.Failure<bool>(ServerErrors.FailureOnRelease(request.Id));
+		}
+	}
+}
